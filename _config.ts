15184a06340056f0a9e5bb9ada22e960585266f4
Jeremy Karlsson <jeremy@barlingshult.se>
2025-09-03 23:07:27 +0200

    Add view-transitions

--- _config.ts
+++ _config.ts
@@ -1,11 +1,13 @@
 import lume from "lume/mod.ts";
 import esbuild from "lume/plugins/esbuild.ts";
 import inline from "lume/plugins/inline.ts";
-import postcss from "lume/plugins/postcss.ts";
-import nunjucks from "lume/plugins/nunjucks.ts";
+import lightningCSS from "lume/plugins/lightningcss.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
+import sitemap from "lume/plugins/sitemap.ts";
 
 const site = lume({
   src: "./src",
+  location: new URL("https://glatek.se"),
 });
 
 site.add("css");
@@ -13,8 +15,9 @@ site.add('/.well-known');
 site.add("fonts");
 
 site.use(esbuild());
-site.use(postcss());
+site.use(lightningCSS());
 site.use(inline());
-site.use(nunjucks());
+site.use(minifyHTML());
+site.use(sitemap());
 
 export default site;
