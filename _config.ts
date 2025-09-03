bfe1e509867ab30e84fd58e497c9532a0782d42f
Jeremy Karlsson <jeremy@barlingshult.se>
2025-09-03 23:13:15 +0200

    Add view transition to footer, remove esbuild

--- _config.ts
+++ _config.ts
@@ -1,5 +1,4 @@
 import lume from "lume/mod.ts";
-import esbuild from "lume/plugins/esbuild.ts";
 import inline from "lume/plugins/inline.ts";
 import lightningCSS from "lume/plugins/lightningcss.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
@@ -14,7 +13,6 @@ site.add("css");
 site.add('/.well-known');
 site.add("fonts");
 
-site.use(esbuild());
 site.use(lightningCSS());
 site.use(inline());
 site.use(minifyHTML());
